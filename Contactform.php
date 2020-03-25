3e20f389d041bd569709fafaedf088940b36a263
unknown <nick.willemsen@kpnmail.nl>
2020-03-25 13:38:26 +0100

    first-commit

---
+++ Contactform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <aside id="sidebar">
+      <div class="dark">
+       <h3>Contact us</h3>
+        <form class="Mail">
+          <div>
+           <label>Name</label><br>
+           <input type="text" placeholder="Name">
+         </div>
+      <div>
+        <label>Email</label><br>
+        <input type="email" placeholder="Email Adress">
+      </div>
+    <div>
+      <label>Message</label><br>
+      <textarea placeholder="Message"></textarea>
+   </div>
+  <button class="button_1" type="submit">Send</button>
+</form>
+    </aside>
+    </div>
+  </body>
+</html>
